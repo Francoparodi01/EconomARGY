37ff322f83ed0c2541fae8d527f641d35b939847
francooparodi01 <francoparodi2001@gmail.com>
2024-12-03 12:54:56 -0300

    Corrigiendo mensajes

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ async def obtener_todos_los_dolares(update: Update, context: CallbackContext):
             mensaje += f"🏠 Dolar: {nombre} \n"
             mensaje += f"💰 Compra: {compra:.2f} ARS\n"
             mensaje += f"💸 Venta: {venta:.2f} ARS\n"
-            mensaje += f"📅 última actualización: {fecha_formateada}"
+            mensaje += f"📅 última actualización: {fecha_formateada}\n\n"
 
         # Verificar el límite de caracteres de Telegram y enviar el mensaje
         if len(mensaje) > 4096:
